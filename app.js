8c4e15405082902fd29d6e4a18c44bcda801986a
Moran <moranfine@gmail.com>
2014-06-05 18:22:21 +0300

    add em-extract directive and embedlyService service

--- app.js
+++ app.js
@@ -7,6 +7,25 @@ var embedlyModule = angular.module('embedlyModule', []);
 embedlyModule.factory('initParams', function(){
     return {
         //here enter key
-        key: 'your key'
+//        key: 'your key'
     }
 })
+
+embedlyModule.service('embedlyService', function($http, initParams){
+    this.checkIfEnter = function(e) {
+        //if enter pressed
+        if(e.keyCode === 13) {
+            this.checkUrl();
+        }
+    };
+    this.checkUrl = function(requestType, urlToSearch) {
+        var embedlyRequest = 'http://api.embed.ly/1/' + requestType + '?key=' + initParams.key + '&url=' +  urlToSearch;
+        $http({method: 'GET', url: embedlyRequest}).
+            success(function(data, status, headers, config) {
+                console.log(data);
+            }).
+            error(function(data, status, headers, config) {
+                console.log('error');
+            });
+    }
+});
