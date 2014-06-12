c165db2c7dfeea041247a4f4746313642134d0fe
Moran <moranfine@gmail.com>
2014-06-12 16:14:32 +0300

    add photo check in emEmbed directive

--- angular-embedly.js
+++ angular-embedly.js
@@ -42,20 +42,30 @@
             },
             link: function(scope, element, attrs, controller) {
                 attrs.$observe('urlsearch', function(newVal) {
+                    var previousEmbedCode = scope.embedCode;
                     if (newVal) {
                         embedlyService.embed(newVal)
                             .then(function(data){
-                                var previousEmbedCode = scope.embedCode;
-                                scope.embedCode = '<div>' + data.data.html + '</div>';
+                                switch(data.data.type) {
+                                    case 'video':
+                                        scope.embedCode = data.data.html;
+                                        break;
+                                    case 'photo':
+                                        scope.embedCode = '<img src="' + data.data.url + '">';
+                                        break;
+                                    default:
+                                        scope.embedCode = '';
+                                }
                                 if(previousEmbedCode !== scope.embedCode) {
-                                    element.html(scope.embedCode);
+                                    // embed code was changed from last call and has to be replaced in DOM
+                                    element.html('<div>' + scope.embedCode + '</div>');
                                 }
                             }, function(error) {
                                 // promise rejected
                                 var previousEmbedCode = scope.embedCode;
-                                scope.embedCode = '<div></div>';
+                                scope.embedCode = '';
                                 if(previousEmbedCode !== scope.embedCode) {
-                                    element.html(scope.embedCode);
+                                    element.html('<div>' + scope.embedCode + '</div>');
                                 }
                             });
                     }
--- examples/input-example/app.js
+++ examples/input-example/app.js
@@ -5,7 +5,7 @@
 var embedlyModule = angular.module('embedlyModule1', ['angular-embedly']);
 
 embedlyModule.config(function(embedlyServiceProvider){
-    embedlyServiceProvider.setKey('your key');
+    embedlyServiceProvider.setKey('66f36234bd244532afc9a6e4f6afa5ec');
 });
 
 function embedCtrl($scope) {
