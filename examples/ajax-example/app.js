c9f38a59f58514166fe1f22fe599d5473a7eb286
Moran <moranfine@gmail.com>
2014-06-12 10:23:31 +0300

    add ajax example

--- examples/ajax-example/app.js
+++ examples/ajax-example/app.js
@@ -8,6 +8,26 @@ embedlyModule.config(function(embedlyServiceProvider){
     embedlyServiceProvider.setKey('your key');
 });
 
-function embedCtrl2($scope) {
-    $scope.urlToSearch = '';
+function embedCtrl2($scope, $http) {
+//    $scope.values = [];
+//    $http.get('values.json').success(function(data) {
+//        $scope.values = data;
+//    });
+    $scope.values = [
+        {
+            "firstName": "Liv",
+            "lastName": "Boeree",
+            "inputUrl": "https://www.youtube.com/watch?v=errZYGLKeIM"
+        },
+        {
+            "firstName": "Annette",
+            "lastName": "Obrestad",
+            "inputUrl": "https://www.youtube.com/watch?v=w_HaaEGstd8"
+        },
+        {
+            "firstName": "Vanessa",
+            "lastName": "Selbst",
+            "inputUrl": "https://www.youtube.com/watch?v=thi4g02W7q4"
+        }
+    ]
 }
