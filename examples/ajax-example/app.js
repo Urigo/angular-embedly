84f951b83220db7468059c47fe5d882692fa9d79
Moran <moranfine@gmail.com>
2014-06-12 10:32:43 +0300

    add http request to ajax example

--- examples/ajax-example/app.js
+++ examples/ajax-example/app.js
@@ -9,25 +9,8 @@ embedlyModule.config(function(embedlyServiceProvider){
 });
 
 function embedCtrl2($scope, $http) {
-//    $scope.values = [];
-//    $http.get('values.json').success(function(data) {
-//        $scope.values = data;
-//    });
-    $scope.values = [
-        {
-            "firstName": "Liv",
-            "lastName": "Boeree",
-            "inputUrl": "https://www.youtube.com/watch?v=errZYGLKeIM"
-        },
-        {
-            "firstName": "Annette",
-            "lastName": "Obrestad",
-            "inputUrl": "https://www.youtube.com/watch?v=w_HaaEGstd8"
-        },
-        {
-            "firstName": "Vanessa",
-            "lastName": "Selbst",
-            "inputUrl": "https://www.youtube.com/watch?v=thi4g02W7q4"
-        }
-    ]
+    $scope.values = [];
+    $http.get('values.json').success(function(data) {
+        $scope.values = data;
+    });
 }
