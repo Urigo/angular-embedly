ecf4da91026db0ba27c9e242b4120090a166b9e4
Diogo Ribeiro <diogoriba@diogoriba.com>
2014-11-29 20:40:35 -0200

    adding flags for compatibility w/ full https websites

--- angular-embedly.js
+++ angular-embedly.js
@@ -10,6 +10,7 @@ var angularEmbedly = angular.module('angular-embedly', []);
 (function (module) {
     module.provider('embedlyService', function () {
         var key;
+        var secure;
         this.setKey = function(userKey) {
             key = userKey;
             return key;
@@ -17,11 +18,21 @@ var angularEmbedly = angular.module('angular-embedly', []);
         this.getKey = function() {
             return key;
         }
+        this.secure = function(value) {
+            if (!value) {
+                return secure;
+            }
+            secure = value;
+        }
+
+        function getProtocol() {
+            return secure ? 'https' : 'https' ;
+        }
 
         function embedly($http) {
             this.embed = function(inputUrl, maxwidth) {
                 var escapedUrl = encodeURI(inputUrl);
-                var embedlyRequest = 'http://api.embed.ly/1/oembed?key=' + key + '&url=' +  escapedUrl;
+                var embedlyRequest = getProtocol() + '://api.embed.ly/1/oembed?key=' + key + '&url=' +  escapedUrl;
 
                 if(typeof maxwidth !== 'undefined'){
                     embedlyRequest = embedlyRequest + '&maxwidth=' + maxwidth;
@@ -31,7 +42,7 @@ var angularEmbedly = angular.module('angular-embedly', []);
             };
             this.extract = function(inputUrl) {
                 var escapedUrl = encodeURI(inputUrl);
-                var embedlyRequest = 'http://api.embed.ly/1/extract?key=' + key + '&url=' +  escapedUrl;
+                var embedlyRequest = getProtocol + '://api.embed.ly/1/extract?key=' + key + '&url=' +  escapedUrl;
                 return $http({method: 'GET', url: embedlyRequest});
             };
         }
