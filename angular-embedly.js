1440a7db7b3a181966ad58f8b111af97b3c5b194
Alexander Kushi-Willis <ackw.kushi@gmail.com>
2015-02-28 23:49:26 -0500

    bug fix

--- angular-embedly.js
+++ angular-embedly.js
@@ -7,58 +7,63 @@ var angularEmbedly = angular.module('angular-embedly', []);
  * Created by moran on 12/06/14.
  */
 
-(function (module) {
-    module.provider('embedlyService', function () {
-        var key;
-        var secure;
-        this.setKey = function(userKey) {
-            key = userKey;
-            return key;
-        }
-        this.getKey = function() {
-            return key;
-        }
-        this.secure = function(value) {
-            if (!value) {
-                return secure;
-            }
-            secure = value;
-        }
+(function(module) {
+  module.provider('embedlyService', function() {
+    var key;
+    var secure;
+    this.setKey = function(userKey) {
+      key = userKey;
+      return key;
+    }
+    this.getKey = function() {
+      return key;
+    }
+    this.secure = function(value) {
+      if (!value) {
+        return secure;
+      }
+      secure = value;
+    }
 
-        function getProtocol() {
-            return secure ? 'https' : 'https' ;
-        }
+    function getProtocol() {
+      return secure ? 'https' : 'https';
+    }
 
-        function embedly($http) {
-            this.embed = function(inputUrl, maxwidth, scheme) {
-                var escapedUrl = encodeURI(inputUrl);
-                var embedlyRequest = getProtocol() + '://api.embed.ly/1/oembed?key=' + key + '&url=' +  escapedUrl;
+    function embedly($http) {
+      this.embed = function(inputUrl, maxwidth, scheme) {
+        var escapedUrl = encodeURI(inputUrl);
+        var embedlyRequest = getProtocol() + '://api.embed.ly/1/oembed?key=' + key + '&url=' + escapedUrl;
 
-                if(typeof maxwidth !== 'undefined'){
-                    embedlyRequest = embedlyRequest + '&maxwidth=' + maxwidth;
-                }
-
-                if(typeof scheme !== 'undefined'){
-                    embedlyRequest = embedlyRequest + '&scheme=' + scheme;
-                }
-
-                return $http({method: 'GET', url: embedlyRequest});
-            };
-            this.extract = function(inputUrl) {
-                var escapedUrl = encodeURI(inputUrl);
-                var embedlyRequest = getProtocol + '://api.embed.ly/1/extract?key=' + key + '&url=' +  escapedUrl;
-                return $http({method: 'GET', url: embedlyRequest});
-            };
+        if (typeof maxwidth !== 'undefined') {
+          embedlyRequest = embedlyRequest + '&maxwidth=' + maxwidth;
         }
 
+        if (typeof scheme !== 'undefined') {
+          embedlyRequest = embedlyRequest + '&scheme=' + scheme;
+        }
 
-        this.$get = ['$http', function($http) {
-            return new embedly($http);
+        return $http({
+          method: 'GET',
+          url: embedlyRequest
+        });
+      };
+      this.extract = function(inputUrl) {
+        var escapedUrl = encodeURI(inputUrl);
+        var embedlyRequest = getProtocol() + '://api.embed.ly/1/extract?key=' + key + '&url=' + escapedUrl;
+        return $http({
+          method: 'GET',
+          url: embedlyRequest
+        });
+      };
+    }
+
+
+    this.$get = ['$http', function($http) {
+      return new embedly($http);
         }];
 
-    })
+  })
 })(angularEmbedly);
-
 /**
  * Created by moran on 12/06/14.
  */
